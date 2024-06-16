388203920a51cfa87192360d8019a4ceb0bd5eef
lsproule <lsproule@gmail.com>
2024-06-16 19:45:38 -0400

    add enabled disabled stuff ig (needs refactored)

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -8,48 +8,47 @@ M.servers = {}
 M.path = ""
 
 M.initial_load = function()
-	for _, file in pairs(vim.fn.readdir(M.path), [[v:val =~ '\.lua$']]) do
-		--vim.notify(M.path .. file)
-		local data = require("lsps." .. file:gsub("%.lua$", ""))
+  for _, file in pairs(vim.fn.readdir(M.path), [[v:val =~ '\.lua$']]) do
+    --vim.notify(M.path .. file)
+    local data = require("lsps." .. file:gsub("%.lua$", ""))
     M.servers[data[1]] = {}
-		--vim.notify(vim.inspect(data))
-		if data.enabled then
-			--vim.notify(vim.inspect(file))
-			M.servers[data[1]] = data
-		end
+    --vim.notify(vim.inspect(data))
+    if data.enabled then
+      --vim.notify(vim.inspect(file))
+      M.servers[data[1]] = data
+    end
     M.servers[data[1]].enabled = data.enabled
-		M.enabled_servers[data[1]] = data.enabled
-	end
+    M.enabled_servers[data[1]] = data.enabled
+  end
 end
 
 M.to_json = function()
-	--local M.enabled_servers = {}
-	local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "w")
-	if not file then
-		vim.notify("Error opening file")
-		return
-	end
-	file:write(vim.json.encode(M.enabled_servers))
+  --local M.enabled_servers = {}
+  local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "w")
+  if not file then
+    vim.notify("Error opening file")
+    return
+  end
+  file:write(vim.json.encode(M.enabled_servers))
 end
 
-
 M.from_json = function()
-	local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "r")
+  local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "r")
 
-	if not file then
+  if not file then
     M.create_json()
-		return
-	end
+    return
+  end
 
-	local data = file:read("*a")
-	if data == nil then
-		vim.notify("No data in file")
-		return
-	end
+  local data = file:read("*a")
+  if data == nil then
+    vim.notify("No data in file")
+    return
+  end
 
-	for server, enabled in pairs(vim.json.decode(data)) do
-	  M.servers[server].enabled = enabled
-	end
+  for server, enabled in pairs(vim.json.decode(data)) do
+    M.servers[server].enabled = enabled
+  end
 end
 
 M.create_json = function()
@@ -62,80 +61,82 @@ M.create_json = function()
 end
 
 M.update_json = function(server)
-	local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "r")
-	if not file then
+  local file = io.open(vim.fn.stdpath("config") .. "/servers.json", "r")
+  if not file then
     M.create_json()
-		return
-	end
-	local data = vim.json.decode(file:read("*a"))
-	if data[server] == nil then
-		data[server] = true
-	elseif data[server] then
-		data[server] = false
-	elseif not data[server] then
-		data[server] = true
-	else
-		data[server] = not data[server]
-	end
-	file = io.open(vim.fn.stdpath("config") .. "/servers.json", "w")
-	if not file then
-		return
-	end
-	file:write(vim.json.encode(data))
-	file:flush()
+    return
+  end
+  local data = vim.json.decode(file:read("*a"))
+  if data[server] == nil then
+    data[server] = true
+  elseif data[server] then
+    data[server] = false
+  elseif not data[server] then
+    data[server] = true
+  else
+    data[server] = not data[server]
+  end
+  file = io.open(vim.fn.stdpath("config") .. "/servers.json", "w")
+  if not file then
+    return
+  end
+  file:write(vim.json.encode(data))
+  file:flush()
 end
 
 local function toggle_server(server)
-	M.update_json(server)
+  M.update_json(server)
 end
 
---TODO: Create an lsp autotesting
-
 M.telescope_toggle = function()
-	local finders = require("telescope.finders")
-	local conf = require("telescope.config").values
-	local telescope_picker = require("telescope.pickers")
-	local server_names = {}
-	for server, _ in pairs(M.servers) do
-		table.insert(server_names, server)
-	end
-	telescope_picker
-		.new({}, {
-			prompt_title = "LSP Servers",
-			finder = finders.new_table({
-				results = server_names,
-			}),
-			sorter = conf.generic_sorter({}),
-			attach_mappings = function(prompt_bufnr, map)
-				local toggle = function()
-					local selection = require("telescope.actions.state").get_selected_entry()
-					toggle_server(selection.value)
-					require("telescope.actions").close(prompt_bufnr)
-				end
-				map("i", "<CR>", toggle)
-				map("n", "<CR>", toggle)
-				return true
-			end,
-		})
-		:find()
+  M.from_json()
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  local telescope_picker = require("telescope.pickers")
+  local server_data = {}
+  for server, _ in pairs(M.servers) do
+    local server_string = "" .. server .. " " .. (M.servers[server].enabled and "enabled" or "disabled")
+    table.insert(server_data, server_string)
+  end
+
+  telescope_picker
+      .new({}, {
+        prompt_title = "LSP Servers",
+        finder = finders.new_table({
+          results = server_data,
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          local toggle = function()
+            local selection = require("telescope.actions.state").get_selected_entry()
+            local server = selection.value:match("(.+)%s")
+            toggle_server(server)
+            require("telescope.actions").close(prompt_bufnr)
+          end
+          map("i", "<CR>", toggle)
+          map("n", "<CR>", toggle)
+          return true
+        end,
+      })
+      :find()
 end
 
 M.setup_servers = function()
-	for server, config in pairs(M.servers) do
-		if M.enabled_servers[server] then
-			lspconfig[server].setup(config)
-		end
-	end
+  for server, config in pairs(M.servers) do
+    if M.enabled_servers[server] then
+      lspconfig[server].setup(config)
+    end
+  end
 end
 
 M.setup = function(opts)
-	M.path = opts.path or vim.fn.stdpath("config") .. "/lua/lsps/"
-	M.initial_load()
+  M.path = opts.path or vim.fn.stdpath("config") .. "/lua/lsps/"
+  M.initial_load()
   M.from_json()
-	M.setup_servers()
-	vim.api.nvim_create_user_command("ToggleServer", function()
-	  M.telescope_toggle()
-	end, {})
+  M.setup_servers()
+  vim.api.nvim_create_user_command("ToggleServer", function()
+    M.telescope_toggle()
+  end, {})
 end
 
 return M
